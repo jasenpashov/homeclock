f67831bb51c499c5b653f539af85bf94cd2af66e
jasen <jasen@revauxy.com>
2025-09-19 11:32:56 +0300

    Fonts

--- App.tsx
+++ App.tsx
@@ -8,13 +8,18 @@ import {
   TouchableOpacity
 } from 'react-native';
 import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+export const Font1 = { fontFamily: 'RobotoMono-Bold' }
+export const Font2 = { fontFamily: 'RobotoCondensed-Bold' }
+export const Font3 = { fontFamily: 'SourceCodePro-Bold' }
+
 
 const App = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
   const [color, setColor] = useState('#00ff00');
   const [backgroundColor, setBackgroundColor] = useState('#000000')
-  const [size, setSize] =useState(360)
+  const [size, setSize] = useState(360)
   const [settingsPage, setSettingsPage] = useState(false);
+  const [myFont, setFont] = useState(Font1);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -33,39 +38,74 @@ const App = () => {
   };
 
   return (
-    <SafeAreaView style={[styles.safeArea,{backgroundColor:backgroundColor}]}>
+    <SafeAreaView style={[styles.safeArea, { backgroundColor: backgroundColor }]}>
       <StatusBar hidden />
-      <View style={[styles.container,{backgroundColor:backgroundColor}]}>
+      <View style={[styles.container, { backgroundColor: backgroundColor }]}>
         {settingsPage ? (
           <View>
-            <Text style={[styles.lable, { color: color }]}>Цвят на текста:</Text>
+            <Text style={[styles.lable, { color: color }]}>Text Color:</Text>
             <TextInput
               value={color}
               onChangeText={setColor}
               style={[styles.inputBox]}
             />
-            <Text style={[styles.lable, { color: color }]}>Цвят на фона:</Text>
+            <Text style={[styles.lable, { color: color, marginTop: 20  }]}>Background Color:</Text>
             <TextInput
               value={backgroundColor}
               onChangeText={setBackgroundColor}
               style={[styles.inputBox]}
             />
-            <Text style={[styles.lable, { color: color }]}>Рамер на шрифта:</Text>
+            <Text style={[styles.lable, { color: color, marginTop: 20  }]}>Font Size:</Text>
             <TextInput
               value={size.toString()}
-              onChangeText={(t)=>setSize(parseInt(t,10))}
+              onChangeText={(t) => setSize(parseInt(t, 10))}
               style={[styles.inputBox]}
             />
+            <Text style={[styles.lable, { color: color, marginTop: 20 }]}>
+              Font Family:
+            </Text>
+            <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop: 10 }}>
+              <TouchableOpacity onPress={() => setFont(Font1)}>
+                <Text style={[
+                  styles.button,
+                  { color: color },
+                  myFont.fontFamily === Font1.fontFamily ? { textDecorationLine: 'underline' } : null
+                ]}>
+                  Font1
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => setFont(Font2)}>
+                <Text style={[
+                  styles.button,
+                  { color: color, marginTop: 20  },
+                  myFont.fontFamily === Font2.fontFamily ? { textDecorationLine: 'underline' } : null
+                ]}>
+                  Font2
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => setFont(Font3)}>
+                <Text style={[
+                  styles.button,
+                  { color: color },
+                  myFont.fontFamily === Font3.fontFamily ? { textDecorationLine: 'underline' } : null
+                ]}>
+                  Font3
+                </Text>
+              </TouchableOpacity>
+            </View>
+
             <TouchableOpacity onPress={() => setSettingsPage(false)}>
-              <Text style={[styles.lable,styles.button, {color: color }]}>
+              <Text style={[styles.lable, styles.button, { color: color }]}>
                 OK
               </Text>
             </TouchableOpacity>
           </View>
         ) : (
-          <TouchableOpacity onPress={() => setSettingsPage(true)}>
-            <Text style={[styles.clockText, { color: color, fontSize:size }]}>{formatHour(currentTime)}</Text>
-            <Text style={[styles.clockText, { color: color, fontSize:size }]}>{formatMinute(currentTime)}</Text>
+          <TouchableOpacity onPress={() => setSettingsPage(true)} style={{ flex: 1, justifyContent: "center", alignItems: "center", width: '100%', }}>
+            <Text style={[styles.clockText, { color: color, fontSize: size, ...myFont }]}>{formatHour(currentTime)}</Text>
+            <Text style={[styles.clockText, { color: color, fontSize: size, ...myFont }]}>{formatMinute(currentTime)}</Text>
           </TouchableOpacity>
         )
         }
@@ -84,9 +124,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   clockText: {
-    fontWeight: 'bold',
-    padding:0,
-    margin:0
+    // ...defaultFont,
+    width: '100%',
+    textAlign: 'center',
+    padding: 0,
+    margin: 0,
+    borderColor: "#fff",
+    borderWidth: 0
   },
   lable: {
     fontSize: 20
